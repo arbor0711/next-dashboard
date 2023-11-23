b59af5284d7b645c2041bf052c81eb98876c2f22
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-11-23 22:53:42 +0100

    Add Footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex  mx-auto -text--textSoft mt-10 mb-5">
+      <p>2023 Alireza.K.Shirazi</p>
+      <div className="divider divider-horizontal"></div>
+      <a
+        href="https://github.com/arbor0711/next-dashboard"
+        target="_blank"
+        className="hover:-text--text"
+      >
+        See code on GitHub
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
