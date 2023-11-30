e04dfad44c0bf1a719ff0a4388d8d2aa58d0abdc
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-11-30 20:19:52 +0100

    Fix layout files

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,26 +2,20 @@ import Sidebar from "../Sidebar";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body>
-        <div className="flex justify-between gap-5 ">
-          <div className="h-screen w-1/5 min-w-fit -bg--bgSoft	">
-            <Sidebar />
-          </div>
+    <div className="flex justify-between gap-5 ">
+      <div className="h-screen w-1/5 min-w-fit -bg--bgSoft	">
+        <Sidebar />
+      </div>
 
-          <div className="w-full flex flex-col gap-5 mt-5 mr-5 ">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
-      </body>
-    </html>
+      <div className="w-full flex flex-col gap-5 mt-5 mr-5 ">
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </div>
   );
-}
+};
+
+export default layout;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,21 +3,29 @@ import ContentBox from "../components/ContentBox";
 
 const LoginPage = () => {
   return (
-    <div className="w-1/2 h-screen m-auto">
-      <ContentBox>
-        <form className="form-control ">
-          <h1 className="h1">Login</h1>
-          <label>Username</label>
-          <input className="input input-bordered" placeholder="username" />
+    <div className="w-screen h-screen grid  place-content-center ">
+      <div className=" w-96 h-fit">
+        <ContentBox>
+          <form className="form-control">
+            <h1 className="text-2xl font-bold my-7 text-center">Login</h1>
+            <label>Username</label>
+            <input
+              className="input input-bordered mb-5 mt-2"
+              placeholder="username"
+            />
 
-          <label>Password</label>
-          <input className="input input-bordered" placeholder="password" />
+            <label>Password</label>
+            <input
+              className="input input-bordered mb-5 mt-2"
+              placeholder="password"
+            />
 
-          <button className="btn btn-primary" type="submit">
-            Login
-          </button>
-        </form>
-      </ContentBox>
+            <button className="btn btn-primary text-lg mb-5" type="submit">
+              Login
+            </button>
+          </form>
+        </ContentBox>
+      </div>
     </div>
   );
 };
